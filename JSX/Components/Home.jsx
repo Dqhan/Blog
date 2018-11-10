d3b7762a43fd15d847e146038f0e44673eea73c5
dqhan <291927006@qq.com>
2018-11-10 20:24:28 +0800

    2133123

--- JSX/Components/Home.jsx
+++ JSX/Components/Home.jsx
@@ -5,6 +5,27 @@ import Banner from '../AUI/Banner';
 export default class Home extends React.Component {
     constructor(props) {
         super(props);
+        this.initState().initBind();
+    }
+
+    initState() {
+        this.state = {
+            source: [
+                { date: { year: '2018', others: '11月10日' }, title: '最新1', description: '曾有个密友问我，她说：“亲爱的，女生到底是应该嫁给爱情，还是嫁给现实呢？嫁给现实，无非就是自己少奋斗几年。而后用10数年的时间去后悔。不过这么说也算牵强，身边不乏些嫁给现实的朋友，在柴米油盐的生活中不仅慢慢习惯上了对方的存在，那种习惯略带些依赖的味道，并在长此以往的依赖中得了一种病' },
+                { date: { year: '2018', others: '11月10日' }, title: '最新1', description: '曾有个密友问我，她说：“亲爱的，女生到底是应该嫁给爱情，还是嫁给现实呢？嫁给现实，无非就是自己少奋斗几年。而后用10数年的时间去后悔。不过这么说也算牵强，身边不乏些嫁给现实的朋友，在柴米油盐的生活中不仅慢慢习惯上了对方的存在，那种习惯略带些依赖的味道，并在长此以往的依赖中得了一种病' },
+                { date: { year: '2018', others: '11月10日' }, title: '最新1', description: '曾有个密友问我，她说：“亲爱的，女生到底是应该嫁给爱情，还是嫁给现实呢？嫁给现实，无非就是自己少奋斗几年。而后用10数年的时间去后悔。不过这么说也算牵强，身边不乏些嫁给现实的朋友，在柴米油盐的生活中不仅慢慢习惯上了对方的存在，那种习惯略带些依赖的味道，并在长此以往的依赖中得了一种病' },
+                { date: { year: '2018', others: '11月10日' }, title: '最新1', description: '曾有个密友问我，她说：“亲爱的，女生到底是应该嫁给爱情，还是嫁给现实呢？嫁给现实，无非就是自己少奋斗几年。而后用10数年的时间去后悔。不过这么说也算牵强，身边不乏些嫁给现实的朋友，在柴米油盐的生活中不仅慢慢习惯上了对方的存在，那种习惯略带些依赖的味道，并在长此以往的依赖中得了一种病' },
+                { date: { year: '2018', others: '11月10日' }, title: '最新1', description: '曾有个密友问我，她说：“亲爱的，女生到底是应该嫁给爱情，还是嫁给现实呢？嫁给现实，无非就是自己少奋斗几年。而后用10数年的时间去后悔。不过这么说也算牵强，身边不乏些嫁给现实的朋友，在柴米油盐的生活中不仅慢慢习惯上了对方的存在，那种习惯略带些依赖的味道，并在长此以往的依赖中得了一种病' },
+                { date: { year: '2018', others: '11月10日' }, title: '最新1', description: '曾有个密友问我，她说：“亲爱的，女生到底是应该嫁给爱情，还是嫁给现实呢？嫁给现实，无非就是自己少奋斗几年。而后用10数年的时间去后悔。不过这么说也算牵强，身边不乏些嫁给现实的朋友，在柴米油盐的生活中不仅慢慢习惯上了对方的存在，那种习惯略带些依赖的味道，并在长此以往的依赖中得了一种病' },
+                { date: { year: '2018', others: '11月10日' }, title: '最新1', description: '曾有个密友问我，她说：“亲爱的，女生到底是应该嫁给爱情，还是嫁给现实呢？嫁给现实，无非就是自己少奋斗几年。而后用10数年的时间去后悔。不过这么说也算牵强，身边不乏些嫁给现实的朋友，在柴米油盐的生活中不仅慢慢习惯上了对方的存在，那种习惯略带些依赖的味道，并在长此以往的依赖中得了一种病' },
+                { date: { year: '2018', others: '11月10日' }, title: '最新1', description: '曾有个密友问我，她说：“亲爱的，女生到底是应该嫁给爱情，还是嫁给现实呢？嫁给现实，无非就是自己少奋斗几年。而后用10数年的时间去后悔。不过这么说也算牵强，身边不乏些嫁给现实的朋友，在柴米油盐的生活中不仅慢慢习惯上了对方的存在，那种习惯略带些依赖的味道，并在长此以往的依赖中得了一种病' },
+            ]
+        }
+        return this;
+    }
+
+    initBind() {
+
     }
 
     componentDidMount() {
@@ -13,6 +34,25 @@ export default class Home extends React.Component {
 
     componentWillReceiveProps(nextProps) {
 
+    }
+
+    renderSource() {
+        return this.state.source.length > 0 && this.state.source.map(s => {
+            return <section className='section-item'>
+                <div className='section-item-date'>
+                    <div className='section-item-date-year'>
+                        {s.date.year}
+                    </div>
+                    <div className='section-item-date-others'>
+                        {s.date.others}
+                    </div>
+                </div>
+                <div>
+                    <div className='section-item-title'>{s.title}</div>
+                    <div className='section-item-description'>{s.description}</div>
+                </div>
+            </section >
+        })
     }
 
     render() {
@@ -33,9 +73,17 @@ export default class Home extends React.Component {
                 </div>
             </header>
             <Banner />
-            <section>
-
-            </section>
+            <div className='home-body'>
+                <div className='sections'>
+                    <div className='content-title'>
+                        <h2>最新动态</h2>
+                    </div>
+                    {this.renderSource()}
+                </div>
+                <div className='summary'>
+                    Logo
+                </div>
+            </div>
             <footer>
 
             </footer>
