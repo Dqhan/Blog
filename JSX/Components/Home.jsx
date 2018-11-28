c0fa244188a628bdd8e0610e907ab977cade8b34
Dqhan <291927006@qq.com>
2018-11-28 20:46:13 +0800

    1111111111111111

--- JSX/Components/Home.jsx
+++ JSX/Components/Home.jsx
@@ -77,28 +77,43 @@ export default class Home extends React.Component {
                     {this.renderSource()}
                 </div>
                 <div className='summary inline-block'>
-
-                    <ul>
-                        <li>
-                            <a href="https://github.com/Dqhan" target="_blank">
-                                <span title='GitHub' style={{ backgroundImage: "url(../../Image/git.png)" }}></span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://www.cnblogs.com/moran1992/" target="_blank">
-                                <span title='博客园' style={{ backgroundImage: "url(../../Image/blog.png)" }}></span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://www.dqhanhouse.com" target="_blank">
-                                <span title='分享' style={{ backgroundImage: "url(../../Image/share.png)" }}></span>
-                            </a>
-                        </li>
-                    </ul>
+                    <section>
+                        <h3>了解更多</h3>
+                        <ul>
+                            <li>
+                                <a href="https://github.com/Dqhan" target="_blank">
+                                    <span title='GitHub' style={{ backgroundImage: "url(../../Image/git.png)" }}></span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.cnblogs.com/moran1992/" target="_blank">
+                                    <span title='博客园' style={{ backgroundImage: "url(../../Image/blog.png)" }}></span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.dqhanhouse.com" target="_blank">
+                                    <span title='分享' style={{ backgroundImage: "url(../../Image/share.png)" }}></span>
+                                </a>
+                            </li>
+                        </ul>
+                    </section>
+                    <section>
+                        <h3>标签集</h3>
+                        <div className='tags'>
+                            <span style={{ background: '#FF0000' }}>Html5</span>
+                            <span style={{ background: '#33FF00' }}>JavaScript</span>
+                            <span style={{ background: '#FF66FF' }}>Css3</span>
+                            <span style={{ background: '#FF9900' }}>React</span>
+                            <span style={{ background: '#00FFFF' }}>MongoDB</span>
+                            <span style={{ background: '#CC66FF' }}>Node</span>
+                            <span style={{ background: '#0000FF' }}>前端构建</span>
+                            <span style={{ background: '#666666' }}>设计模式</span>
+                        </div>
+                    </section>
                 </div>
             </div>
             <footer>
-
+                <div>暂时没有</div>
             </footer>
         </div>
     }
--- Layouts/Nav.jsx
+++ Layouts/Nav.jsx
@@ -10,16 +10,14 @@ export default class Nav extends React.Component {
 
     render() {
         return <div className='layout-nav'>
-            <div className='nav-container'>
-                <ul className='nav-ul'>
-                    <li className='nav-item'><Link to='/'>首页</Link></li>
-                    <li className='nav-item'><Link to='/sub/lifemark'>生活</Link></li>
-                    <li className='nav-item'><Link to='/sub/workmark'>工作</Link></li>
-                    <li className='nav-item'><Link to='/sub/blog'>博客</Link></li>
-                    <li className='nav-item'><Link to='/sub/production'>作品</Link></li>
-                    <li className='nav-item'><Link to='/sub/about'>关于</Link></li>
-                </ul>
-            </div>
+            <ul className='nav-ul'>
+                <li className='nav-item'><Link to='/'>首页</Link></li>
+                <li className='nav-item'><Link to='/sub/lifemark'>生活</Link></li>
+                <li className='nav-item'><Link to='/sub/workmark'>工作</Link></li>
+                <li className='nav-item'><Link to='/sub/blog'>博客</Link></li>
+                <li className='nav-item'><Link to='/sub/production'>作品</Link></li>
+                <li className='nav-item'><Link to='/sub/about'>关于</Link></li>
+            </ul>
         </div>
     }
 }
