199686808e23c551c85e3e23c286f9a4210d2b55
dqhan <291927006@qq.com>
2018-12-02 22:25:18 +0800

    11111

--- Layouts/Index.jsx
+++ Layouts/Index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import Summary from '../JSX/Components/Summary';
 import Header from '../JSX/Components/Header';
-
+import Summary from '../JSX/Components/Summary';
 export default class Layout extends React.Component {
     constructor(props) {
         super(props);
